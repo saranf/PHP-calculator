adcc3edeb0158f17c4e435393be1311b5a056995
qorwlsdk1996 <qorwlsdk1996@gmail.com>
2019-03-20 21:37:46 +0900

    test Get

--- index.php
+++ index.php
@@ -3,4 +3,19 @@
 	var_dump($_GET);
 
 echo $_GET["AC"];
+echo $_GET["="];
+echo $_GET["/"];
+echo $_GET["*"];
+echo $_GET["-"];
+echo $_GET["+"];
+echo $_GET["9"];
+echo $_GET["8"];
+echo $_GET["7"];
+echo $_GET["6"];
+echo $_GET["5"];
+echo $_GET["4"];
+echo $_GET["3"];
+echo $_GET["2"];
+echo $_GET["1"];
+
 ?>
