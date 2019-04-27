82189dc9a2f74794c820b19c93f0a889f1e3b5da
qorwlsdk1996 <qorwlsdk1996@gmail.com>
2019-04-27 20:30:24 +0900

    change isset function

--- index_session.php
+++ index_session.php
@@ -2,13 +2,11 @@
 session_start();
 
 error_log(print_r($_SESSION),0);
-if (empty($_SESSION['number'])){
-		$_SESSION['number'] =$_POST['0'];
-		error_log("에러레요",0);
+if (isset($_SESSION['number'])){
+		$_SESSION['number'] =$_SESSION['number']."".$_POST['1'];
 	}
 else{
-	$_SESSION['number']=$_SESSION['number']."". $_POST['1']."".$_POST['2']."".$_POST['3']."".$_POST['4']."".$_POST['5'];
-	error_log("에러레염",0);
+	$_SESSION['number']= $_POST['0'];
 }
 
 if($_POST['AC']=="AC")
