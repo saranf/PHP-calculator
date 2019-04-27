e56bcdcece1b8557ea49f150941470db7dd72856
qorwlsdk1996 <qorwlsdk1996@gmail.com>
2019-04-27 22:28:32 +0900

    한글자 지우기

--- index_session.php
+++ index_session.php
@@ -1,10 +1,7 @@
 <?php
 session_start();
 
-$test = $_POST['number'];
-error_log(count($test),0);
 if (isset($_SESSION['number'])){
-
 	if($_SESSION['number']==0){
 		unset($_SESSION['number']);
 		$_SESSION['number']=$_POST['number'][0];
@@ -17,6 +14,15 @@ else{
 	$_SESSION['number']= $_POST['number'][0];
 
 		
+}
+
+if($_POST['eraser']=="eraser"){
+	$session_encode = $_SESSION['number'];
+	$_SESSION['number']=substr($session_encode,0,-1);
+
+//
+//$session['number']=$session_decode;
+error_log($session_decode,0);	
 }
 
 if($_POST['AC']=="AC")
