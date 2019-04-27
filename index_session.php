ef9bcd1166c5e807101ce1cc870e55f9609f729d
qorwlsdk1996 <qorwlsdk1996@gmail.com>
2019-04-27 22:40:07 +0900

    5글자 이상시 숫자 더 안늘어남

--- index_session.php
+++ index_session.php
@@ -2,14 +2,22 @@
 session_start();
 
 if (isset($_SESSION['number'])){
-	if($_SESSION['number']==0){
-		unset($_SESSION['number']);
-		$_SESSION['number']=$_POST['number'][0];
+	if(strlen($_SESSION['number']) < 5){
+		error_log("test",0);
+		if($_SESSION['number']==0){
+			unset($_SESSION['number']);
+			$_SESSION['number']=$_POST['number'][0];
+		}
+		else{
+			$_SESSION['number']= $_SESSION['number']."".$_POST['number'][0];
+		}
 	}
 	else{
-		$_SESSION['number']= $_SESSION['number']."".$_POST['number'][0];
+		header('location:./index.php');
 	}
+
 }
+
 else{
 	$_SESSION['number']= $_POST['number'][0];
 
@@ -20,8 +28,6 @@ if($_POST['eraser']=="eraser"){
 	$session_encode = $_SESSION['number'];
 	$_SESSION['number']=substr($session_encode,0,-1);
 
-//
-//$session['number']=$session_decode;
 error_log($session_decode,0);	
 }
 
