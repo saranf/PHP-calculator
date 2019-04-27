e6b3d177837d2441d78cd792649402d7db152a4a
qorwlsdk1996 <qorwlsdk1996@gmail.com>
2019-04-27 21:04:54 +0900

    test count array

--- index_session.php
+++ index_session.php
@@ -1,17 +1,13 @@
 <?php
 session_start();
 
-error_log(print_r($_SESSION),0);
+error_log(count($_POST['number']),0);
 if (isset($_SESSION['number'])){
 
-	//$_SESSION['number']= $_SESSION['number']."".$_POST['0']."".$_POST['1']."".$_POST['2']."".$_POST['3']."".$_POST['4']."".$_POST['5']."".$_POST['6']."".$_POST['7']."".$_POST['8']."".$_POST['9'];
 	$_SESSION['number']= $_SESSION['number']."".$_POST['number'][0];
 
 	}
 else{
-//	$_SESSION['number']= $_POST['0']."".$_POST['1']."".$_POST['2']."".$_POST['3']."".$_POST['4']."".$_POST['5']."".$_POST['6']."".$_POST['7']."".$_POST['8']."".$_POST['9'];
-
-
 	$_SESSION['number']= $_POST['number'][0];
 }
 
