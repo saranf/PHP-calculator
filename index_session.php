57b92e522579b64ddda30be91ecc3177af6a6e49
qorwlsdk1996 <qorwlsdk1996@gmail.com>
2019-04-27 20:37:21 +0900

    add POST number

--- index_session.php
+++ index_session.php
@@ -3,10 +3,14 @@ session_start();
 
 error_log(print_r($_SESSION),0);
 if (isset($_SESSION['number'])){
-		$_SESSION['number'] =$_SESSION['number']."".$_POST['1'];
+
+	$_SESSION['number']= $_SESSION['number']."".$_POST['0']."".$_POST['1']."".$_POST['2']."".$_POST['3']."".$_POST['4']."".$_POST['5']."".$_POST['6']."".$_POST['7']."".$_POST['8']."".$_POST['9'];
+
 	}
 else{
-	$_SESSION['number']= $_POST['0'];
+	$_SESSION['number']= $_POST['0']."".$_POST['1']."".$_POST['2']."".$_POST['3']."".$_POST['4']."".$_POST['5']."".$_POST['6']."".$_POST['7']."".$_POST['8']."".$_POST['9'];
+
+
 }
 
 if($_POST['AC']=="AC")
