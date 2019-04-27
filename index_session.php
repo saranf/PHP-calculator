ca35403f7412bda8b2981a53db2a20fb9f5f6029
qorwlsdk1996 <qorwlsdk1996@gmail.com>
2019-04-27 21:10:20 +0900

    test count array

--- index_session.php
+++ index_session.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-error_log(count($_POST['number']),0);
+error_log(count($_POST['number'][0]),0);
 if (isset($_SESSION['number'])){
 
 	$_SESSION['number']= $_SESSION['number']."".$_POST['number'][0];
