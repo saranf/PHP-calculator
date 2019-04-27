ae561e891a9effb00bd0267e4d25b1cffcc0f1ba
qorwlsdk1996 <qorwlsdk1996@gmail.com>
2019-04-27 21:00:42 +0900

    test array

--- index_session.php
+++ index_session.php
@@ -5,14 +5,14 @@ error_log(print_r($_SESSION),0);
 if (isset($_SESSION['number'])){
 
 	//$_SESSION['number']= $_SESSION['number']."".$_POST['0']."".$_POST['1']."".$_POST['2']."".$_POST['3']."".$_POST['4']."".$_POST['5']."".$_POST['6']."".$_POST['7']."".$_POST['8']."".$_POST['9'];
-	$_SESSION['number']= $_SESSION['number']."".$_POST['number'];
+	$_SESSION['number']= $_SESSION['number']."".$_POST['number'][0];
 
 	}
 else{
 //	$_SESSION['number']= $_POST['0']."".$_POST['1']."".$_POST['2']."".$_POST['3']."".$_POST['4']."".$_POST['5']."".$_POST['6']."".$_POST['7']."".$_POST['8']."".$_POST['9'];
 
 
-	$_SESSION['number']= $_POST['number'];
+	$_SESSION['number']= $_POST['number'][0];
 }
 
 if($_POST['AC']=="AC")
