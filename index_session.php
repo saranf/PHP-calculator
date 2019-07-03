1049232883ae67de9e3e3b2d4a66762b356532ce
saranf <qorwlsdk1995@naver.com>
2019-07-03 19:34:29 +0900

    덧셈 에러 고침

--- index_session.php
+++ index_session.php
@@ -3,23 +3,20 @@ session_start();
 
 $session_number=0;
 
+if (isset($_SESSION['number'])){ //number 문자가 들어올 경우에
 
-if (isset($_SESSION['number'])){
-	if($_SESSION['equal']){
-		$_SESSION['equal'] = FALSE;
-		$_SESSION['number'] = '';
-	}
-
-	if($_SESSION['is_opt']){
+	if($_SESSION['is_opt']){ //부호 값이 들어올시 그 전 숫자 저장 그 후 숫자 초기화
 		$_SESSION['is_opt'] = FALSE;
 		$_SESSION['old_number'] = $_SESSION['number'];
 		$_SESSION['number'] = '';
 	}
 
-	if(strlen($_SESSION['number']) < 5){
-		if($_SESSION['number']==0){
-			unset($_SESSION['number']);
-			$_SESSION['number']=$_POST['number'][0];
+	if(strlen($_SESSION['number']) < 5){ //숫자가 5자 미만이면
+		if($_SESSION['number'] == null){ //null 값이 있는 상태로 0이 들어오면 다시 들어온다.
+			if($_SESSION['number']==0){
+				unset($_SESSION['number']);
+				$_SESSION['number']=$_POST['number'][0];
+			}
 		}
 
 		else{
@@ -45,21 +42,15 @@ else{
 if($_POST['eraser']=="eraser"){
 	$session_encode = $_SESSION['number'];
 	$_SESSION['number']=substr($session_encode,0,-1);
-
-	//error_log($session_decode,0);
 }
 
 if ($_POST['+']=='+'){
-//	if($_SESSION['is_opt']==FALSE){
-//		$_SESSION['number'] = $_SESSION['old_number'] + $_SESSION['number'];
-//	}
 
-	if(isset($_SESSION['sign'])){
-		$_SESSION['number'] = func_plus($_SESSION['old_number'], $_SESSION['number']);
+	if(isset($_SESSION['sign'])){//부호가 활성화 되면
+		$_SESSION['number'] = func_plus($_SESSION['old_number'], $_SESSION['number']); // 더하기를 하고 number에 넣는다.
 		$_SESSION['old_number'] = null;
 	}else{
 		$_SESSION['sign'] ='+';
-		$_SESSION['third'] = TRUE;
 		$_SESSION['is_opt'] = TRUE;
 		$session_number = $_SESSION['old_number'] =  $_POST['number'];
 	}
@@ -69,13 +60,6 @@ if ($_POST['+']=='+'){
 
 
 if ($_POST['-']=='-'){
-
-//	if($_SESSION['is_opt']==FALSE){
-//		$_SESSION['number'] = $_SESSION['old_number'] - $_SESSION['number'];
-//	}
-
-
-
 	$_SESSION['sign'] = '-';
 	$_SESSION['third'] = TRUE;
 	$_SESSION['is_opt'] = TRUE;
@@ -113,10 +97,10 @@ if ($_POST['%']=='%'){
 
 
 if( $_POST["="] == '='){
-	$_SESSION['sign'] = null;  // 연산자 초기화
-	$_SESSION['equal'] = TRUE;
 	if($_SESSION['sign'] =='+'){
 		$_SESSION['number'] = $_SESSION['old_number'] + $_SESSION['number'];
+		$_SESSION['sign'] = null;  // 연산자 초기화
+		//$_SESSION['equal'] = TRUE;
 	}
 	if($_SESSION['sign'] == '-'){
 		$_SESSION['number'] = $_SESSION['old_number'] - $_SESSION['number'];
